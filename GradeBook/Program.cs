2bb2f1419b78f38f59d44b7f0db52380cbd41047
stanmozolevskiy <stanmozolevskiy@gmail.com>
2020-03-08 21:27:45 -0400

    first part

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace GradeBook
 {
@@ -6,7 +7,22 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // var a = 3.14;
+            // var b = 5.20;
+            // var numbers = new[] { 3.14, 4.25, 6.20 };
+
+            var book = new Book("My grade book");
+            book.AddGrade(8.2);
+            book.AddGrade(3.14);
+            book.AddGrade(20.6);
+             
+            
+            var stats = book.GetStats();
+            Console.WriteLine($"The Lowest grade is {stats.Low}");
+            Console.WriteLine($"The Highest grade is {stats.High}");
+            Console.WriteLine($"The Average grade is {stats.Average}");
+
+
         }
     }
 }

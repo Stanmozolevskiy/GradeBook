d9dd828f024add180936dc0085d9b5a7b30baaac
stanmozolevskiy <stanmozolevskiy@gmail.com>
2020-03-08 22:25:05 -0400

    Third part

--- GradeBook/Program.cs
+++ GradeBook/Program.cs
@@ -7,20 +7,40 @@ namespace GradeBook
     {
         static void Main(string[] args)
         {
-            // var a = 3.14;
-            // var b = 5.20;
-            // var numbers = new[] { 3.14, 4.25, 6.20 };
+
 
             var book = new Book("My grade book");
-            book.AddGrade(8.2);
-            book.AddGrade(3.14);
-            book.AddGrade(20.6);
-             
-            
+            while (true)
+            {
+                Console.WriteLine("Please enter the grade or press q to see the statistics");
+                var value = Console.ReadLine();
+                    
+                if (value == "q")
+                    {
+                    break;
+                    }
+                try
+                {
+                book.AddGrade(double.Parse(value));
+
+                }
+                catch(ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (FormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
             var stats = book.GetStats();
             Console.WriteLine($"The Lowest grade is {stats.Low}");
             Console.WriteLine($"The Highest grade is {stats.High}");
             Console.WriteLine($"The Average grade is {stats.Average}");
+            Console.WriteLine($"The Letter grade is {stats.Letter}");
+
+
 
 
         }
